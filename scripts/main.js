27b3c5585f425ccb6eedeff26c915070cf3d4884
Bedrich Bubik <xbubik00@stud.fit.vutbr.cz>
2012-11-12 00:28:49 +0100

    added close button for photos

--- scripts/main.js
+++ scripts/main.js
@@ -3,12 +3,23 @@ window.onload = function()
 	document.getElementsByName('add_media')[0].onclick = function() {new SlideshowElement()};	
 };
 
+function close (me) {
+	me.parentNode.parentNode.removeChild(me.parentNode);
+}
+
 function SlideshowElement() 
 {
-	this.el = document.createElement('div');
+	var photo = document.createElement('div');
+	var button = document.createElement('div');
+	photo.className = 'media_element';
+	button.onclick = function() {close(this)};
+	button.className = 'delete_button';
+	photo.appendChild(button);
+	document.getElementById('media_canvas').appendChild(photo);
+	/*this.el = document.createElement('div');
 	this.el.className = 'media_element';
 
 	this.parent_el = document.getElementById('media_canvas');
-	this.parent_el.appendChild(this.el);
+	this.parent_el.appendChild(this.el);*/
 
 }
