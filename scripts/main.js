27a099ff5012365bd83b1cfec28f8f9710d2a1b6
Ondrej Hujnak <xhujna00@stud.fit.vutbr.cz>
2012-11-12 18:36:44 +0100

    Added QueryWindow object

--- scripts/main.js
+++ scripts/main.js
@@ -11,9 +11,12 @@ function SlideshowElement()
 {
 	var photo = document.createElement('div');
 	var button = document.createElement('div');
+
 	photo.className = 'media_element';
+
 	button.onclick = function() {close(this)};
 	button.className = 'delete_button';
+	
 	photo.appendChild(button);
 	document.getElementById('media_canvas').appendChild(photo);
 	/*this.el = document.createElement('div');
@@ -23,3 +26,43 @@ function SlideshowElement()
 	this.parent_el.appendChild(this.el);*/
 
 }
+
+function QueryWindow()
+{
+	this.bg = document.createElement('div');
+	this.bg.id = 'grey_out';
+	this.bg.style.height = window.innerHeight + 'px';
+
+	this.createWindow('10em', '20em');
+
+	document.body.appendChild(this.bg);
+}
+
+QueryWindow.prototype = 
+{
+	createWindow: function(height, width)
+	{
+		var wnd = document.createElement('div');
+		wnd.id = 'query_window';
+		wnd.style.height = height;
+		wnd.style.width = width;
+		
+		var close = document.createElement('div');
+		close.className = 'delete_button';
+		var t = this;
+		close.onclick = function() {t.close()};
+		wnd.appendChild(close);
+
+		wnd.appendChild(this.createForm());
+
+		this.bg.appendChild(wnd);
+	},
+	createForm: function()
+	{
+		return document.createElement('form');
+	},
+	close: function()
+	{
+		this.bg.parentNode.removeChild(this.bg);
+	}
+};
