8ee44fab2540164223851657ea1348e89a9e2bc8
Ondrej Hujnak <xhujna00@stud.fit.vutbr.cz>
2012-11-22 13:50:23 +0100

    Added simple google map

--- scripts/visualiser.js
+++ scripts/visualiser.js
@@ -13,5 +13,27 @@ onLoad.add( function(modifyer) {
 
 	var Map = new google.maps.Map(document.getElementById('map'), startOpt);
 
+	google.maps.event.addListener(Map, "center_changed", function(ev) {
+		// log Map.getCenter()
+	});
+
+	google.maps.event.addListener(Map, "zoom_changed", function(ev) {
+		// log Map.getZoom()
+	});
+
+	google.maps.event.addListener(Map, "maptypeid_changed", function(ev) {
+		// log Map.getZoom()
+		alert(Map.getMapTypeId());
+	});
+
+	var contextMenu = new ContextMenu(Map, {
+		classNames: {menu: 'context_menu'},
+		menuItems: [{eventName: 'zoom_in_click', label: 'Add place'}]
+	});
+
+	google.maps.event.addListener(Map, "rightclick", function(ev) {
+		contextMenu.show(ev.latLng);
+	});
+
 });
 
