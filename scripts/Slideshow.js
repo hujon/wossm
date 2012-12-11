727b44148832fc665ec91e2ec17abd424428ed44
Ondrej Hujnak <xhujna00@stud.fit.vutbr.cz>
2012-12-11 20:34:46 +0100

    Select places, reassign photos

---
+++ scripts/Slideshow.js
@@ -0,0 +1,211 @@
+/// Slideshow Element
+
+var SlideshowElement = Obj.create({
+  constructor : function(file) {
+    var type = file.type.split('/')[0];
+    try {
+      return eval("new SlideshowElement_" + type + "(file)");
+    } catch(e) {};
+    
+  },
+  el : null,
+  name : null,
+  remove : function() {
+    this.el.parentNode.removeChild(this.el);
+  },
+  display : function() {
+  }
+}); // var SlideshowElement
+
+var SlideshowElement_image = Obj.extend(SlideshowElement, {
+  constructor : function(file) {
+
+    this.name = file.name;
+    var _this = this;
+    
+    var reader = new FileReader();
+
+    reader.onload = function(content) {
+        _this.src = content.target.result;
+        _this.display(); 
+    };
+
+    reader.readAsDataURL(file);
+  },
+  src : null,
+  display : function()
+  {
+    var el = $('<div/>', {
+      "class"     : 'media_element',
+      draggable : true
+    }).appendTo('#media_canvas');
+    this.el = el[0];
+
+    $('<div/>', {
+      "class"   : 'delete_button',
+      click     : function() {this.remove()},
+      draggable : false
+    }).appendTo(el);
+
+    $('<img/>', {
+      "class"   : "thumbnail",
+      title     : escape(this.name),
+      src       : this.src,
+      draggable : false
+    }).appendTo(el);
+
+    var _this = this;
+    this.el.addEventListener('dragstart', function(evt) {
+        evt.dataTransfer.effectAllowed = 'copy';
+        evt.dataTransfer.setData('application/json', JSON.stringify(_this));
+        dragItem = _this;
+        this.style.opacity = '0.5';
+    }, false);
+
+    this.el.addEventListener('dragend', function(evt) {
+      this.style.opacity = '1';
+      dragItem = null;
+    }, false);
+  }
+});
+
+var Place = Obj.create({
+  constructor : function(map, name, lat, lng) {
+    this.media = new Array();
+    this.map = map;
+    this.name = name;
+
+    this.marker = this.displayOnMap(lat, lng);
+
+    this.el = this.displayPlace();
+
+    this.selectEntry = this.createSelectEntry();
+
+  },
+  map : null,
+  name : null,
+  marker : null,
+  el : null,
+  selectEntry : null,
+  media : null,
+  getPosition : function() 
+  {
+    return this.marker.getPosition();
+  },
+  displayOnMap  : function(lat, lng)
+  {
+    return new google.maps.Marker({
+      draggable : true,
+      labelText: 'test',
+      position : new google.maps.LatLng(lat, lng),
+      map : this.map
+    });
+  },
+  displayPlace  : function()
+  {
+    var _this = this;
+    container = $( '<div/>', {
+      "class" : "place"
+    }).appendTo('#places');
+    var el = container[0];
+
+    $('<input type="text" />').val(this.name).change( function() {
+      _this.name = this.value;
+    }).appendTo(container);
+
+    var droparea = $('<div />', {
+      "class"   : 'droparea',
+      draggable : true,
+      click     : function()
+      {
+        input = $(_this.el).find('input');
+        if(!input.is(":focus")) {
+          var tmp = input.val();
+          input.val('');
+          input.focus();
+          input.val(tmp);
+        }
+
+      }
+    }).appendTo(container);
+    droparea = droparea[0];
+    
+    droparea.addEventListener('dragenter', function(evt) {
+      _this.el.classList.add('dragOver');
+    }, false);
+
+    droparea.addEventListener('dragover', function(evt) {
+      evt.preventDefault();
+      evt.dataTransfer.dropEffect = 'copy';
+      return false;
+    }, false);
+    
+    droparea.addEventListener('dragleave', function(evt) {
+      _this.el.classList.remove('dragOver');
+    }, false);
+
+    droparea.addEventListener('drop', function(evt) {
+      evt.preventDefault();
+      evt.stopPropagation();
+      _this.el.classList.remove('dragOver');
+      if(evt.dataTransfer.getData('application/json')) {
+        if(activePlace != null) {
+          Slideshow.places[activePlace].media.splice(
+            Slideshow.places[activePlace].media.indexOf(dragItem),
+            1
+          );
+        } else {
+          Slideshow.unPlaced.splice(
+            Slideshow.unPlaced.indexOf(dragItem),
+            1
+          );
+        }
+        _this.media.push(dragItem);
+      }
+      if(activePlace != null) {
+        Slideshow.places[activePlace].displayMedia();
+      } else {
+        Slideshow.displayUnPlaced();
+      }
+      return false;
+    }, false);
+
+    return el;
+  },
+  createSelectEntry : function()
+  {
+    el = $('<option />', {
+      value : Slideshow.places.length,
+      text  : this.name
+    }).appendTo('#placeSelector select');
+    return el;
+  },
+  displayMedia  : function()
+  {
+    $('#media_canvas').html('');
+
+    for(var i = 0; i < this.media.length; i++) {
+      this.media[i].display();
+    }
+  }
+}); // var Place
+
+var Slideshow = {
+  places : new Array(),
+  unPlaced : new Array(),
+  addMedia : function(media) 
+  {
+    this.unPlaced.push(media);
+  },
+  addPlace : function(place)
+  {
+    this.places.push(place);
+  },
+  displayUnPlaced : function()
+  {
+    $('#media_canvas').html('');
+    for(var i=0; i<this.unPlaced.length; i++) {
+      this.unPlaced[i].display();
+    }
+  }
+} // var Slideshow
--- scripts/main.js
+++ scripts/main.js
@@ -55,158 +55,26 @@ var Obj = {
 }; // var Obj
 
 
-var dragItem = null;
-/// Slideshow Element
+dragItem = null;
+activePlace = null;
 
-var SlideshowElement = Obj.create({
-  constructor : function(file) {
-    var type = file.type.split('/')[0];
-    try {
-      return eval("new SlideshowElement_" + type + "(file)");
-    } catch(e) {};
-    
-  },
-  el : null,
-  name : null,
-  remove : function() {
-    this.el.parentNode.removeChild(this.el);
-  }
-}); // var SlideshowElement
+$('#placeSelector').ready( function() {
+  $('#placeSelector select').change( function() {
+    activePlace = this.value || null;
 
-var SlideshowElement_image = Obj.extend(SlideshowElement, {
-  constructor : function(file) {
+    var len = $('#placeSelector option:selected').text().length;
 
-    this.name = file.name;
-    var _this = this;
-    
-    var reader = new FileReader();
-
-    reader.onload = function(content) {
-      var el = $('<div/>', {
-        "class"     : 'media_element',
-        draggable : true
-      }).appendTo('#media_canvas');
-      _this.el = el[0];
-
-      $('<div/>', {
-        "class"   : 'delete_button',
-        click     : function() {_this.remove()},
-        draggable : false
-      }).appendTo(el);
-
-      $('<img/>', {
-        "class"   : "thumbnail",
-        title     : escape(file.name),
-        src       : content.target.result,
-        draggable : false
-      }).appendTo(el);
-
-      _this.el.addEventListener('dragstart', function(evt) {
-          evt.dataTransfer.effectAllowed = 'copy';
-          evt.dataTransfer.setData('application/json', JSON.stringify(_this));
-          dragItem = _this;
-          this.style.opacity = '0.5';
-      }, false);
-
-      _this.el.addEventListener('dragend', function(evt) {
-          this.style.opacity = '1';
-        dragItem = null;
-      }, false);
-    
-    };
-
-    reader.readAsDataURL(file);
-  }
-});
-
-var Place = Obj.create({
-  constructor : function(map, name, lat, lng) {
-    this.media = new Array();
-    this.map = map;
-    this.name = name;
-    var _this = this;
-
-    this.marker = new google.maps.Marker({
-      draggable : true,
-      labelText: 'test',
-      position : new google.maps.LatLng(lat, lng),
-      map : this.map
+    $('#placeSelector').css({
+      'width' : len + 3 + 'em'
     });
-
-    container = $( '<div/>', {
-      "class" : "place"
-    }).appendTo('#places');
-    this.el = container[0];
-
-    $('<input type="text" />').val(this.name).change( function() {
-      _this.name = this.value;
-    }).appendTo(container);
-
-    var droparea = $('<div />', {
-      "class"   : 'droparea',
-      draggable : true,
-      click     : function()
-      {
-        input = $(_this.el).find('input');
-        if(!input.is(":focus")) {
-          var tmp = input.val();
-          input.val('');
-          input.focus();
-          input.val(tmp);
-        }
-
-      }
-    }).appendTo(container);
-    droparea = droparea[0];
+    $('#placeSelector select').css({
+      'width' : len + 3 + 2+'em'
+    });
     
-    droparea.addEventListener('dragenter', function(evt) {
-      _this.el.classList.add('dragOver');
-    }, false);
-
-    droparea.addEventListener('dragover', function(evt) {
-      evt.preventDefault();
-      evt.dataTransfer.dropEffect = 'copy';
-      return false;
-    }, false);
-    
-    droparea.addEventListener('dragleave', function(evt) {
-      _this.el.classList.remove('dragOver');
-    }, false);
-
-    droparea.addEventListener('drop', function(evt) {
-      evt.preventDefault();
-      evt.stopPropagation();
-      _this.el.classList.remove('dragOver');
-      if(evt.dataTransfer.getData('application/json')) {
-        var index = Slideshow.unPlaced.indexOf(dragItem);
-        if(index >= 0) {
-          Slideshow.unPlaced.splice(index, 1);
-        }
-        _this.media.push(dragItem);
-      }
-      return false;
-    }, false);
-
-  },
-  map : null,
-  name : null,
-  marker : null,
-  el : null,
-  media : null,
-  getPosition : function() {
-    return this.marker.getPosition();
-  }
-}); // var Place
-
-var Slideshow = {
-  places : new Array(),
-  unPlaced : new Array(),
-  addMedia : function(media) 
-  {
-    this.unPlaced.push(media);
-  },
-  addPlace : function(place)
-  {
-    this.places.push(place);
-  }
-} // var Slideshow
+    if(activePlace == null) {
+      Slideshow.displayUnPlaced();
+    } else {
+      Slideshow.places[activePlace].displayMedia();
+    }
+  });
+});
